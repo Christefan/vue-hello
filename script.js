4b1d29ac9fc6e50a2a40b906f9036b2e8efbc0b2
Christefan <chriscapod7@gmail.com>
2022-05-05 17:32:29 +0200

    Prova funzionamento vue

---
+++ script.js
@@ -0,0 +1,11 @@
+Vue.config.devtools = true;
+
+const app = new Vue(
+    {
+        el: '#root',
+        data:{
+            title:'Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.',
+            imgRdm:'https://unsplash.it/300/300?image='
+        }
+    }
+)
